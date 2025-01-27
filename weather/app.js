1a0c0a1bb7982ee58b86b8060599dc2de47bd8b4
vinayak2109 <vinayakphuge6@gmail.com>
2025-01-27 15:25:34 +0530

    Updated app.js

--- weather/app.js
+++ weather/app.js
@@ -1,6 +1,6 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+ var createError = require('http-errors');
+var express = require('expres');
+var path = require('path')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
